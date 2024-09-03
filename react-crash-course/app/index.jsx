1e94cb3822ea2a9b3d55824cb2395266c6f8543a
kiqsmg <kiqsmg@gmail.com>
2024-09-02 21:43:54 -0300

    Change the secondary colors to a ton of blue, add floripasat2 image and rebuild the index page

--- react-crash-course/app/index.jsx
+++ react-crash-course/app/index.jsx
@@ -13,34 +13,26 @@ export default function App() {
       <ScrollView contentContainerStyle={{ height: '100%'}}>
         <View className="w-full justify-center items-center min-h-[85px] px-4">
           <Image 
-            source={images.logo}
-            className="w-[130px] h-[84px]"
-            resizeMode="contain"
-          />
-
-          <Image 
-            source={images.cards}
-            className="max-w-[380px] w-full h-[300px]"
+            source={images.floripasat2}
+            className="w-[200px] h-[300px] mt-10 "
             resizeMode="contain"
           />
 
           <View className="relative mt-5">
-            <Text className="text-3xl text-white font-bold text-center">
-              Discover Endless{"\n"}
-              Possibilities with{" "}
-              <Text className="text-secondary-200">Aora</Text>
+            <Text className="text-2xl text-secondary-400 font-bold text-center">
+            <Text className="text-4xl text-white">NanoSatTracker</Text>{"\n"}
+              Making Satellite Data Accessible
             </Text>
 
             <Image
               source={images.path}
-              className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+              className="w-[180px] h-[15px] absolute -bottom-2 -right-8"
               resizeMode="contain"
             />
           </View>
 
           <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
-            Where Creativity Meets Innovation: Embark on a Journey of Limitless
-            Exploration with Aora
+            Where Satellites and Data Converge: Understand Space Data Like Never Before with NanoSatTrack.
           </Text>
 
           <CustomButton
