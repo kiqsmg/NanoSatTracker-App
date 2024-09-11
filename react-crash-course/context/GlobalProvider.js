7756e97740904b5402403c973d529eaf526ac2e0
kiqsmg <kiqsmg@gmail.com>
2024-09-11 15:53:16 -0300

    Add firebase starting code and logout to the globalprovider, so the layout and index files will import auth from here

--- react-crash-course/context/GlobalProvider.js
+++ react-crash-course/context/GlobalProvider.js
@@ -1,6 +1,8 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged, signOut } from "@firebase/auth";
+import { useRouter } from 'expo-router';
+import { auth } from "../lib/fireabaseConfig";
 
-import { getCurrentUser } from "../lib/appwrite";
 
 const GlobalContext = createContext();
 export const useGlobalContext = () => useContext(GlobalContext);
@@ -9,36 +11,36 @@ const GlobalProvider = ({ children }) => {
   const [isLogged, setIsLogged] = useState(false);
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+  const router = useRouter();
 
   useEffect(() => {
-    getCurrentUser()
-      .then((res) => {
-        if (res) {
-          setIsLogged(true);
-          setUser(res);
-        } else {
-          setIsLogged(false);
-          setUser(null);
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsLogged(true);
+        setUser(user);
+      } else {
+        setIsLogged(false);
+        setUser(null);
+      }
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
   }, []);
 
+  const logout = async () => {
+    try {
+      await signOut(auth);
+      setIsLogged(false);
+      setUser(null);
+      router.replace('/index');
+    } catch (error) {
+      console.error("Logout error:", error);
+      // You can show an alert or notification to the user here if needed
+    }
+  };
   return (
-    <GlobalContext.Provider
-      value={{
-        isLogged,
-        setIsLogged,
-        user,
-        setUser,
-        loading,
-      }}
-    >
+    <GlobalContext.Provider value={{ isLogged, setIsLogged, user, setUser, loading, logout }}>
       {children}
     </GlobalContext.Provider>
   );
