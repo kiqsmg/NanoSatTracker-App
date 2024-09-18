2b626f801ec32bd3dc3bb3d6046beb9d8a47ed63
kiqsmg <kiqsmg@gmail.com>
2024-09-18 10:47:38 -0300

    Add a third chart and put the 3 charts wrapped in a scrollview

--- react-crash-course/app/(tabs)/create.jsx
+++ react-crash-course/app/(tabs)/create.jsx
@@ -1,10 +1,12 @@
 import React, { useRef } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, ScrollView } from 'react-native';
 import { LineChart } from "react-native-gifted-charts";
 
 const Create = () => {
   const ref1 = useRef(null);
   const ref2 = useRef(null);
+  const ref3 = useRef(null);
+
 
   const lineData1 = [
     {value: 4, label: '1 Jan'},
@@ -68,8 +70,41 @@ const Create = () => {
     {value: 14, label: '30 Jul'},
   ];
 
+  const lineData3 = [
+    {value: 4, label: '1 Jan'},
+    {value: 14, label: '10 Jan'},
+    {value: 8, label: '20 Jan'},
+    {value: 38, label: '30 Jan'},
+    {value: 36, label: '1 Feb'},
+    {value: 28, label: '10 Feb'},
+    {value: 14, label: '20 Feb'},
+    {value: 28, label: '28 Feb'},
+    {value: 4, label: '1 Mar'},
+    {value: 14, label: '10 Mar'},
+    {value: 8, label: '20 Mar'},
+    {value: 14, label: '30 Mar'},
+    {value: 8, label: '1 Apr'},
+    {value: 38, label: '10 Apr'},
+    {value: 14, label: '20 Apr'},
+    {value: 28, label: '30 Apr'},
+    {value: 4, label: '1 May'},
+    {value: 10, label: '10 May'},
+    {value: 8, label: '20 May'},
+    {value: 14, label: '30 May'},
+    {value: 8, label: '1 Jun'},
+    {value: 38, label: '10 Jun'},
+    {value: 14, label: '20 Jun'},
+    {value: 28, label: '30 Jun'},
+    {value: 4, label: '1 Jul'},
+    {value: 28, label: '10 Jul'},
+    {value: 4, label: '20 Jul'},
+    {value: 14, label: '30 Jul'},
+  ];
+
   const months1 = ['Jan','Feb','Mar','Apr','May','Jun','Jul'];
   const months2 = ['Jan','Feb','Mar','Apr','May','Jun','Jul'];
+  const months3 = ['Jan','Feb','Mar','Apr','May','Jun','Jul'];
+
 
   const showOrHidePointer1 = (index) => {
     ref1.current?.scrollTo({
@@ -83,60 +118,95 @@ const Create = () => {
     });
   };
 
+  const showOrHidePointer3 = (index) => {
+    ref2.current?.scrollTo({
+      x: index * 200 - 25, // Adjust according to your UI
+    });
+  };
+
   return (
-    <View>
-      {/* First chart and month selection */}
-      <View style={{ flexDirection: 'row', marginLeft: 8 }}>
-        {months1.map((month, index) => (
-          <TouchableOpacity
-            key={index}
-            style={{
-              padding: 6,
-              margin: 4,
-              backgroundColor: '#ebb',
-              borderRadius: 8,
-            }}
-            onPress={() => showOrHidePointer1(index)}
-          >
-            <Text>{month}</Text>
-          </TouchableOpacity>
-        ))}
+    <ScrollView>
+      <View>
+        {/* First chart and month selection */}
+        <View style={{ flexDirection: 'row', marginLeft: 8 }}>
+          {months1.map((month, index) => (
+            <TouchableOpacity
+              key={index}
+              style={{
+                padding: 6,
+                margin: 4,
+                backgroundColor: '#ebb',
+                borderRadius: 8,
+              }}
+              onPress={() => showOrHidePointer1(index)}
+            >
+              <Text>{month}</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+
+        <LineChart
+          scrollRef={ref1}
+          data={lineData1}
+          curved
+          initialSpacing={0}
+          rotateLabel
+        />
+
+        {/* Second chart and month selection */}
+        <View style={{ flexDirection: 'row', marginLeft: 8, marginTop: 50 }}>
+          {months2.map((month, index) => (
+            <TouchableOpacity
+              key={index}
+              style={{
+                padding: 6,
+                margin: 4,
+                backgroundColor: '#ebb',
+                borderRadius: 8,
+              }}
+              onPress={() => showOrHidePointer2(index)}
+            >
+              <Text>{month}</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+
+        <LineChart
+          scrollRef={ref2}
+          data={lineData2}
+          curved
+          initialSpacing={0}
+          rotateLabel
+        />
+
+        {/* Second chart and month selection */}
+        <View style={{ flexDirection: 'row', marginLeft: 8, marginTop: 50 }}>
+          {months2.map((month, index) => (
+            <TouchableOpacity
+              key={index}
+              style={{
+                padding: 6,
+                margin: 4,
+                backgroundColor: '#ebb',
+                borderRadius: 8,
+              }}
+              onPress={() => showOrHidePointer3(index)}
+            >
+              <Text>{month}</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+
+        <LineChart
+          scrollRef={ref3}
+          data={lineData3}
+          curved
+          initialSpacing={0}
+          rotateLabel
+        />
       </View>
-
-      <LineChart
-        scrollRef={ref1}
-        data={lineData1}
-        curved
-        initialSpacing={0}
-        rotateLabel
-      />
-
-      {/* Second chart and month selection */}
-      <View style={{ flexDirection: 'row', marginLeft: 8, marginTop: 50 }}>
-        {months2.map((month, index) => (
-          <TouchableOpacity
-            key={index}
-            style={{
-              padding: 6,
-              margin: 4,
-              backgroundColor: '#ebb',
-              borderRadius: 8,
-            }}
-            onPress={() => showOrHidePointer2(index)}
-          >
-            <Text>{month}</Text>
-          </TouchableOpacity>
-        ))}
-      </View>
-
-      <LineChart
-        scrollRef={ref2}
-        data={lineData2}
-        curved
-        initialSpacing={0}
-        rotateLabel
-      />
-    </View>
+    </ScrollView>
+    
   );
 };
 
