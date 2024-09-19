360a7d85eb44fbf7e505e4638f56cbeb27e53421
kiqsmg <kiqsmg@gmail.com>
2024-09-19 13:21:18 -0300

    Add new imports from test data

--- react-crash-course/app/(tabs)/create.jsx
+++ react-crash-course/app/(tabs)/create.jsx
@@ -2,7 +2,7 @@ import React, { useRef } from 'react';
 import { View, Text, TouchableOpacity, ScrollView } from 'react-native';
 import { LineChart } from "react-native-gifted-charts";
 
-import lineData1_filtered from '../../state/data_Test';
+import { lineData1_filtered, lineData2_filtered } from '../../state/data_Test';
 
 const Create = () => {
   const ref1 = useRef(null);
@@ -12,36 +12,7 @@ const Create = () => {
 
   const lineData1 = lineData1_filtered
 
-  const lineData2 = [
-    {value: 4, label: '1 Jan'},
-    {value: 14, label: '10 Jan'},
-    {value: 8, label: '20 Jan'},
-    {value: 38, label: '30 Jan'},
-    {value: 36, label: '1 Feb'},
-    {value: 28, label: '10 Feb'},
-    {value: 14, label: '20 Feb'},
-    {value: 28, label: '28 Feb'},
-    {value: 4, label: '1 Mar'},
-    {value: 14, label: '10 Mar'},
-    {value: 8, label: '20 Mar'},
-    {value: 14, label: '30 Mar'},
-    {value: 8, label: '1 Apr'},
-    {value: 38, label: '10 Apr'},
-    {value: 14, label: '20 Apr'},
-    {value: 28, label: '30 Apr'},
-    {value: 4, label: '1 May'},
-    {value: 10, label: '10 May'},
-    {value: 8, label: '20 May'},
-    {value: 14, label: '30 May'},
-    {value: 8, label: '1 Jun'},
-    {value: 38, label: '10 Jun'},
-    {value: 14, label: '20 Jun'},
-    {value: 28, label: '30 Jun'},
-    {value: 4, label: '1 Jul'},
-    {value: 28, label: '10 Jul'},
-    {value: 4, label: '20 Jul'},
-    {value: 14, label: '30 Jul'},
-  ];
+  const lineData2 = lineData2_filtered
 
   const lineData3 = [
     {value: 4, label: '1 Jan'},
@@ -122,7 +93,7 @@ const Create = () => {
           scrollRef={ref1}
           data={lineData1}
           curved
-          initialSpacing={20}
+          initialSpacing={20} 
           rotateLabel
         />
 
