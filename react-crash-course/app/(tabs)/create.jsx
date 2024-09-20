644ae5fcfeb0aed7ca2afbc74b45fd7c1068dd7d
kiqsmg <kiqsmg@gmail.com>
2024-09-20 14:54:25 -0300

    Fix styling problems with chart labels and spaings

--- react-crash-course/app/(tabs)/create.jsx
+++ react-crash-course/app/(tabs)/create.jsx
@@ -29,14 +29,57 @@ const Create = () => {
   ///////////////////////////////              QUAL A DIFERENÇA ENTRE USAR CLASSNAME={}       E UTILIZAR STYLE={{}}
   return (
     <ScrollView>
-      <View className="mt-10">
-        <View className="mb-5">
+      <View className="mt-10 ml-4">
+        <View className="mb-5 mt-10">
           <Text className="text-2xl text-secondary-400 font-bold text-center">
             Solar Panels chart:
           </Text>
         </View>
         {/* First chart and month selection */}
-        <View style={{ flexDirection: 'row', marginLeft: 8, marginBottom: 5,}}>
+        <View style={{ flexDirection: 'row', marginLeft: 8, marginBottom: 10,}}>
+          {months1.map((month, index) => (
+            <TouchableOpacity
+              key={index}
+              style={{
+                padding: 6,
+                margin: 4,
+                backgroundColor: '#86abe1',
+                borderRadius: 8,
+              }}
+              onPress={() => showOrHidePointer1(index)}
+            >
+              <Text>{month}</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+
+        <LineChart
+          scrollRef={ref1}
+          data={lineData1}
+          data2={lineData2}
+          data3={lineData3}
+          data4={lineData4}
+          curved
+          color1="skyblue"
+          color2="orange"
+          color3="red"
+          color4='green'
+          initialSpacing={20}
+          maxValue={0.6} // Valor máximo no eixo Y
+          yAxisOffset={-0.05}
+          rotateLabel
+          noOfSections={5}
+          xAxisLabelsVerticalShift={15}
+        />
+
+
+<View className="mb-5  mt-10">
+          <Text className="text-2xl text-secondary-400 font-bold text-center">
+            Solar Panels chart:
+          </Text>
+        </View>
+        {/* First chart and month selection */}
+        <View style={{ flexDirection: 'row', marginLeft: 8, marginBottom: 10,}}>
           {months1.map((month, index) => (
             <TouchableOpacity
               key={index}
