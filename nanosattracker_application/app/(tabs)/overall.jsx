49f350787b94c59b70e722e01415378e767585f6
kiqsmg <kiqsmg@gmail.com>
2024-09-26 16:06:33 -0300

    Change folder name

--- nanosattracker_application/app/(tabs)/overall.jsx
+++ nanosattracker_application/app/(tabs)/overall.jsx
@@ -1,4 +1,4 @@
-import { View, Text, Image, StyleSheet, TouchableOpacity, ScrollView, ImageBackground, Linking } from 'react-native';
+import { View, Text, Image, ScrollView } from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
