9ee5e3b8214f35a0cd7692c518a9ee5af74d2ca1
kiqsmg <kiqsmg@gmail.com>
2024-09-30 15:59:57 -0300

    Refactor app auth layout file

--- nanosattracker_application/app/(auth)/_layout.jsx
+++ nanosattracker_application/app/(auth)/_layout.jsx
@@ -1,9 +1,6 @@
 import { Redirect, Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 
-//import { Loader } from "../../components";
-//import { useGlobalContext } from "../../context/GlobalProvider";
-
 const AuthLayout = () => {
   
   return (
@@ -22,8 +19,6 @@ const AuthLayout = () => {
           }}
         />
       </Stack>
-
-
       <StatusBar backgroundColor="#161622" style="light" />
     </>
   );
