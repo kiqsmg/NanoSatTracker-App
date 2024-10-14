c848fbbf71e896a231ca115c51471c18d07fea2d
kiqsmg <kiqsmg@gmail.com>
2024-10-14 17:42:35 -0300

    Add worldMap filtered image from github

--- nanosattracker_application/constants/images.js
+++ nanosattracker_application/constants/images.js
@@ -3,5 +3,6 @@ import logo from "../assets/images/logo.png";
 import logoSmall from "../assets/images/logo-small.png";
 import floripasat2 from "../assets/images/FloripaSat2.png";
 import profile from "../assets/images/profile.jpg";
+import worldMap from "../assets/images/worldMap.png";
 
-export default { path, logo, logoSmall, floripasat2, profile };
+export default { path, logo, logoSmall, floripasat2, profile, worldMap };
