1adf69aee76a7ee12d57886735325f1ac9ae9f5b
kiqsmg <kiqsmg@gmail.com>
2024-10-17 09:23:33 -0300

    Refactor auth/sign-up.jsx file

--- nanosattracker_application/app/(auth)/sign-up.jsx
+++ nanosattracker_application/app/(auth)/sign-up.jsx
@@ -12,7 +12,6 @@ import { auth } from '../../lib/fireabaseConfig.js';
 
 
 const SignUp = () => {
-
   const [isSubmitting, setIsSubmitting] = useState(false);
 
   const [form, setForm] = useState({
@@ -20,7 +19,6 @@ const SignUp = () => {
     email: "",
     password: "",
   });
-
 
   const submit = async () => {
     if (!form.username || !form.email || !form.password) {
@@ -34,11 +32,8 @@ const SignUp = () => {
       const userCredential = await createUserWithEmailAndPassword( auth, form.email, form.password );
       const user = userCredential.user;
       console.log('User created:', user);
-      
-      // You can add additional logic here to store the username if needed.
 
       // Navigate to home page after successful sign-up
-
       router.replace("/home");
     } catch (error) {
       Alert.alert("O código tá parando aqui!!!!");
@@ -63,18 +58,15 @@ const SignUp = () => {
               resizeMode="contain"
               className="w-[250px] h-[50px]"
             />
-
             <Text className="text-2xl font-semibold text-white mt-10 font-psemibold">
               Sign up to NanosatTracker
             </Text>
-
             <FormField 
               title="Username"
               value={form.username}
               handleChangeText={(e) => setForm({ ...form, username: e })}
               otherStyles="mt-10"
             />
-
             <FormField
               title="Email"
               value={form.email}
@@ -82,21 +74,18 @@ const SignUp = () => {
               otherStyles="mt-7"
               keyboardType="email-address"
             />
-
             <FormField 
               title="Password"
               value={form.password}
               handleChangeText={(e) => setForm({ ...form, password: e })}
               otherStyles="mt-7"
             />
-
             <CustomButton 
               title="Sign up"
               handlePress={submit}
               containerStyles="mt-7"
               isLoading={isSubmitting}
             />
-
             <View className="flex justify-center pt-5 flex-row gap-2">
               <Text className="text-lg text-gray-100 font-pregular">
                 Have an account already?
