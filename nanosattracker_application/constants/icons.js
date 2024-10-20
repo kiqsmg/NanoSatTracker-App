2e8748204e434560ef0d3e0c6a22d75478373bf6
kiqsmg <kiqsmg@gmail.com>
2024-10-20 12:15:06 -0300

    Refactor icons imports

--- nanosattracker_application/constants/icons.js
+++ nanosattracker_application/constants/icons.js
@@ -1,33 +1,23 @@
-import satellite from "../assets/icons/satellite.png";
-import home from "../assets/icons/home.png";
 import chart_1 from "../assets/icons/chart_1.png";
 import chart_2 from "../assets/icons/chart_2.png";
-import profile from "../assets/icons/profile.png";
-import leftArrow from "../assets/icons/left-arrow.png";
-import menu from "../assets/icons/menu.png";
-import search from "../assets/icons/search.png";
-import upload from "../assets/icons/upload.png";
-import rightArrow from "../assets/icons/right-arrow.png";
-import logout from "../assets/icons/logout.png";
+import community from "../assets/icons/community.png";
 import eyeHide from "../assets/icons/eye-hide.png";
 import eye from "../assets/icons/eye.png";
-import play from "../assets/icons/play.png";
-import community from "../assets/icons/community.png";
+import home from "../assets/icons/home.png";
+import leftArrow from "../assets/icons/left-arrow.png";
+import profile from "../assets/icons/profile.png";
+import satellite from "../assets/icons/satellite.png";
+import search from "../assets/icons/search.png";
 
 export default {
-  play,
-  satellite,
-  home,
   chart_1,
   chart_2,
-  profile,
-  leftArrow,
-  menu,
-  search,
-  upload,
-  rightArrow,
-  logout,
+  community,  
   eyeHide,
-  eye,
-  community,
+  eye,  
+  home,
+  leftArrow,
+  profile,
+  satellite,
+  search,
 };
