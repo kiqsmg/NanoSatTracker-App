31daf76f10a8e34f9f80f39679f263cff739470a
kiqsmg <kiqsmg@gmail.com>
2024-10-20 12:07:39 -0300

    Refactor profile page code

--- nanosattracker_application/app/(tabs)/profile.jsx
+++ nanosattracker_application/app/(tabs)/profile.jsx
@@ -1,4 +1,4 @@
-import { View, Text, Image, TouchableOpacity, ScrollView} from 'react-native';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useGlobalContext } from '../../context/GlobalProvider.js';
