3610b6aae7ad02ba231078ddc02a73b11f850cd4
kiqsmg <kiqsmg@gmail.com>
2025-01-06 02:59:52 -0300

    Update Expo lib, and delete unecessary image

--- nanosattracker_application/constants/images.js
+++ nanosattracker_application/constants/images.js
@@ -1,8 +1,7 @@
-import path from "../assets/images/path.png";
 import logo from "../assets/images/logo.png";
 import logoSmall from "../assets/images/logo-small.png";
 import floripasat2 from "../assets/images/FloripaSat2.png";
 import profile from "../assets/images/profile.jpg";
 import worldMap from "../assets/images/worldMap.png";
 
-export default { path, logo, logoSmall, floripasat2, profile, worldMap };
+export default { logo, logoSmall, floripasat2, profile, worldMap };
